00a2ffe940ab4fc9cd79837953bf12eb740f5ee4
BenFleck25 <ben.fleck2508@outlook.com>
2025-12-03 10:35:24 +0000

    add CORS to app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express') //import express into project
+const cors = require('cors')
 const app = express() //assign functionality to variable
 const fruitRouter = require("./routes/fruits")
 const logger = require("./logger")
 
+app.use(cors())
 app.use(logger)
 app.use(express.json())
+
 
 app.get('/', (req, res) => { // '/' is the endpoint (home), req = request, res=response
   res.send("Welcome to the Fruity API")
